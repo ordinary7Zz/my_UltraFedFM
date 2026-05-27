e410c49854cb7ec9903dbaba3fb882e2d014a180
instincts <2606391015@qq.com>
2026-05-27 19:04:45 +0800

    init

--- scripts/my_class_pretrain_FTCPTC.sh
+++ scripts/my_class_pretrain_FTCPTC.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python main_diagnosis.py \
     --model vit_base_patch16 \
     --batch_size 32 \
-    --epochs 20 \
+    --epochs 10 \
     --nb_classes 2 \
     --data_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/Malignant_ultrasound_images_cropped/by_FTCPTC_train \
     --finetune ./output_dir/checkpoint.pth \
