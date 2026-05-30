4368450b820b6bdac747b7dd4faf5b1014047b22
instincts <2606391015@qq.com>
2026-05-30 23:26:56 +0800

    init

--- scripts/my_binary_segmentation_pretrain.sh
+++ scripts/my_binary_segmentation_pretrain.sh
@@ -10,6 +10,6 @@ CUDA_VISIBLE_DEVICES='1' python main_binary_segmentation.py \
     --test_mask_path "${TEST_MASK_PATH}" \
     --savepath ./output_dir/${DATASET} \
     --batch_size 96 \
-    --epoch 10 \
+    --epoch 20 \
     --note vit_b_ssl_usffm \
     --pretrained ./output_dir/pretrained_ultrafedfm/log_2024-07-16_13_53_08/checkpoint.pth
